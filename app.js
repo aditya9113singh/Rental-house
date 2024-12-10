a60e9ea610d4bf757a10a376c1527e55f7029953
aditya9113singh <136881534+aditya9113singh@users.noreply.github.com>
2024-12-10 12:59:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
+require("dotenv").config();
+
 const express=require("express");
 const app=express();
 const mongoose=require("mongoose");
 const path=require("path");
 const methodOverride=require("method-override");
 
+const port=process.env.PORT||8008;
 
 const expressError=require("./utility/expressError.js");
 
@@ -73,6 +76,6 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({extended: true}));
 
 
-app.listen(8000,()=>{
+app.listen(port,()=>{
     console .log("working");
 })
